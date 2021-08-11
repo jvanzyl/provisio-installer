1836ab12950698e4bb6d5c009c956eed9c2129bc
Jason van Zyl <jason@vanzyl.ca>
2021-08-11 10:22:38 -0400

    Fix jenv post-install script

--- tools/jenv/post-install.sh
+++ tools/jenv/post-install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 provisioFunctions=${1}
+profileYaml=${2}
 installLocation=${8}
 
 source ${provisioFunctions}
-create_variables $profile
+create_variables ${profileYaml}
 JENV_ROOT=${HOME}/.jenv
 mkdir -p ${JENV_ROOT}/plugins 2>&1
 
