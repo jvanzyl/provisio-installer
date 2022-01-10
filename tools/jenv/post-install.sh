aad43c08e9f6356eb505ad7dfbe87241d24d568a
Jason van Zyl <jason.vanzyl@mandiant.com>
2022-01-10 16:29:26 -0500

    Allow jenv to add JDK upon initial provisioning on mac and linux

--- tools/jenv/post-install.sh
+++ tools/jenv/post-install.sh
@@ -3,6 +3,7 @@
 provisioFunctions=${1}
 profileYaml=${2}
 installLocation=${8}
+os=${9}
 profileBinaryDirectory=${11}
 
 source ${provisioFunctions}
@@ -20,14 +21,21 @@ done
 # TODO: (multiple-runtimes) Here we are introducing an implicit relationship of jenv on java. The descriptor
 # should be changed to reflect this and also order the installation of tools as java needs to be installed
 # before jenv can add JDKs
+
+if [ "${os}" = "Darwin" ]; then
+  jdkHome="Contents/Home"
+elif [ "${os}" = "Linux" ]; then
+  jdkHome=""
+fi
+
 for jdk in $(ls ${profileBinaryDirectory}/java)
 do
-  ${JENV_ROOT}/bin/jenv add "${profileBinaryDirectory}/java/${jdk}/Contents/Home"
+  ${JENV_ROOT}/bin/jenv add "${profileBinaryDirectory}/java/${jdk}/${jdkHome}"
 done
 
 for jdk in $(ls ${profileBinaryDirectory}/graalvm)
 do
-  ${JENV_ROOT}/bin/jenv add "${profileBinaryDirectory}/graalvm/${jdk}/Contents/Home"
+  ${JENV_ROOT}/bin/jenv add "${profileBinaryDirectory}/graalvm/${jdk}/${jdkHome}"
 done
 
 [ ! -z "${tools_jenv_global}" ] && ${JENV_ROOT}/bin/jenv global ${tools_jenv_global}
